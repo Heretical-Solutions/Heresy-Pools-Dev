58a38a1012d94e39b5b6f53fedf82b587228a620
Benevolent Drone <benevolentdrone@gmail.com>
2023-03-05 16:36:38 +0200

    Finished sample game object pool

--- Pools/Allocation callbacks/PushToPoolCallback.cs
+++ Pools/Allocation callbacks/PushToPoolCallback.cs
@@ -2,14 +2,56 @@ namespace HereticalSolutions.Pools.AllocationCallbacks
 {
     public class PushToPoolCallback<T> : IAllocationCallback<T>
     {
-        public INonAllocPool<T> Root { get; set; }
+        private INonAllocPool<T> root; 
+        public INonAllocPool<T> Root
+        {
+            get => root;
+            set
+            {
+                root = value;
+
+                if (deferredCallbackQueue != null)
+                {
+                    deferredCallbackQueue.Process();
+                    
+                    deferredCallbackQueue.Callback = null;
+
+                    deferredCallbackQueue = null;
+                }
+            }
+        }
+
+        private DeferredCallbackQueue<T> deferredCallbackQueue;
+
+        public PushToPoolCallback(INonAllocPool<T> root = null)
+        {
+            this.root = root;
+
+            deferredCallbackQueue = null;
+        }
+        
+        public PushToPoolCallback(DeferredCallbackQueue<T> deferredCallbackQueue)
+        {
+            root = null;
+            
+            this.deferredCallbackQueue = deferredCallbackQueue;
+
+            this.deferredCallbackQueue.Callback = this;
+        }
 
         public void OnAllocated(IPoolElement<T> currentElement)
         {
             if (currentElement.Value == null)
                 return;
 
-            Root.Push(currentElement);
+            if (root == null)
+            {
+                deferredCallbackQueue?.Enqueue(currentElement);
+
+                return;
+            }
+
+            root.Push(currentElement);
         }
     }
 }
