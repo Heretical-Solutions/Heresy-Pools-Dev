fcfcb3da5159f63cf48b8c8810a1399704edf9d2
Benevolent Drone <benevolentdrone@gmail.com>
2023-03-14 06:18:41 +0200

    Fixed non alloc message bus

--- Pools/Factories/ElementsFactory.cs
+++ Pools/Factories/ElementsFactory.cs
@@ -139,12 +139,26 @@ namespace HereticalSolutions.Pools.Factories
 				? allocationDelegate.Invoke()
 				: default(T);
 		}
+		
+		public static TResult FuncAllocationDelegate<TResult, TValue>(Func<TValue> allocationDelegate) where TValue : TResult
+		{
+			TValue result = (allocationDelegate != null)
+				? allocationDelegate.Invoke()
+				: default(TValue);
+
+			return (TResult)result;
+		}
 
 		public static T ActivatorAllocationDelegate<T>()
 		{
 			return (T)Activator.CreateInstance(typeof(T));
 		}
 		
+		public static TResult ActivatorAllocationDelegate<TResult, TValue>()
+		{
+			return (TResult)Activator.CreateInstance(typeof(TValue));
+		}
+
 		public static object ActivatorAllocationDelegate(Type valueType)
 		{
 			return Activator.CreateInstance(valueType);
